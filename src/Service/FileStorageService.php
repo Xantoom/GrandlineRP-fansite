b6b4c5689ca7557c3fe932e9d350e206ffa0a8a2
Xantoom <xantoom@gmail.com>
2025-05-19 00:55:57 +0200

    Refactor FileStorageService: add constructor for MinIO configuration parameters

--- src/Service/FileStorageService.php
+++ src/Service/FileStorageService.php
@@ -4,4 +4,12 @@ namespace App\Service;
 
 class FileStorageService
 {
+	public function __construct(
+		string $accessKey,
+		string $secretKey,
+		string $endpoint,
+		string $bucket,
+	)
+	{
+	}
 }
